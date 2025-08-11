cee84049123fcc29f1c2e383b316be87d6e62e1a
Wyatt Lloyd <Wyatt.Lloyd@gmail.com>
2025-08-11 07:49:33 -0400

    Update min_cost_flow.py

--- min_cost_flow.py
+++ min_cost_flow.py
@@ -3,7 +3,7 @@ from typing import Tuple, List
 
 import numpy as np
 import pandas as pd
-from ortools.graph import pywrapgraph
+from ortools.graph.python import min_cost_flow
 
 DIGITS = 2
 
@@ -34,24 +34,27 @@ class MatchingGraph:
         self.num_courses = len(course_info.index)
         self.slots = int(course_info['Slots'].fillna(
             0).replace(r'\s+', 0, regex=True).sum())
+
         source, sink = range(
             self.num_students + self.num_courses + self.slots,
-            2 + self.num_students + self.num_courses + self.slots)
-        self.flow = pywrapgraph.SimpleMinCostFlow(sink + 1)
+            2 + self.num_students + self.num_courses + self.slots,
+        )
+        self.flow = min_cost_flow.SimpleMinCostFlow()
 
         # Each student cannot fill >1 course slot
         for i, w in enumerate(student_weights):
-            self.flow.AddArcWithCapacityAndUnitCost(
+            self.flow.add_arc_with_capacity_and_unit_cost(
                 source, i, 1, -int(w * 10 ** DIGITS))
 
         # Each course slot cannot have >1 TA
         node = self.num_students + self.num_courses
         for i, (_, row) in enumerate(course_info.iterrows()):
             for s in range(int(row['Slots'])):
-                self.flow.AddArcWithCapacityAndUnitCost(
+                self.flow.add_arc_with_capacity_and_unit_cost(
                     self.num_students + i, node, 1, -fill_value(
                         s, row['Base weight'], row['First weight']))
-                self.flow.AddArcWithCapacityAndUnitCost(node, sink, 1, 0)
+                self.flow.add_arc_with_capacity_and_unit_cost(
+                    node, sink, 1, 0)
                 node += 1
 
         # Force fixed matching edges to be filled
@@ -59,19 +62,17 @@ class MatchingGraph:
         for _, row in fixed_matches.iterrows():
             si, ci = row["Student index"], row["Course index"]
             if si == -1:
-                # self.flow.AddArcWithCapacityAndUnitCost(empty, students + ci, 1, 0)
-                self.flow.SetNodeSupply(
-                    self.num_students + ci, self.flow.Supply(
+                self.flow.set_node_supply(
+                    self.num_students + ci, self.flow.supply(
                         self.num_students + ci) + 1)
             elif ci == -1:
                 missing += 1
             else:
                 match_cost = -int(match_weights[si, ci] * 10 ** DIGITS)
-                # must include weight from incoming edge to student node
-                assign_cost = -int(student_weights[si] * 10 ** DIGITS)
-                self.flow.AddArcWithCapacityAndUnitCost(
+                assign_cost = -int(student_weights.iloc[si] * 10 ** DIGITS)
+                self.flow.add_arc_with_capacity_and_unit_cost(
                     si, self.num_students + ci, 1, match_cost + assign_cost)
-                self.flow.SetNodeSupply(si, 1)
+                self.flow.set_node_supply(si, 1)
 
         # Edge weights given by preferences
         for si in range(self.num_students):
@@ -79,44 +80,40 @@ class MatchingGraph:
                 for ci in range(self.num_courses):
                     if not np.isnan(match_weights[si, ci]):
                         cost = -int(match_weights[si, ci] * 10 ** DIGITS)
-                        self.flow.AddArcWithCapacityAndUnitCost(
+                        self.flow.add_arc_with_capacity_and_unit_cost(
                             si, self.num_students + ci, 1, cost)
 
         # Attempt to fill max number of slots
-        self.flow.SetNodeSupply(
+        self.flow.set_node_supply(
             source, int(
                 min(self.num_students, self.slots) - len(
                     fixed_matches.index) + missing))
-        self.flow.SetNodeSupply(sink, -int(min(self.num_students, self.slots)))
+        self.flow.set_node_supply(sink, -int(min(self.num_students, self.slots)))
 
         # Option for not maximizing number of matches
-        self.flow.AddArcWithCapacityAndUnitCost(
+        self.flow.add_arc_with_capacity_and_unit_cost(
             source, sink, int(
                 min(
                     self.num_students, self.slots)), 0)
 
     def solve(self):
-        return self.flow.Solve() == self.flow.OPTIMAL
+        return self.flow.solve() == self.flow.OPTIMAL
 
     def get_matching(self, fixed_matches: pd.DataFrame, weights: np.ndarray) -> \
             List[Tuple[int, int]]:
-        """Returns list of (si, ci) matches"""
         matches = []
         fixed_matches = fixed_matches[['Student index', 'Course index']]
-        for arc in range(self.flow.NumArcs()):
-            si = self.flow.Tail(arc)
-            ci = self.flow.Head(arc) - self.num_students
-            # arcs from student to course
-            if self.flow.Flow(arc) > 0 and si < self.num_students:
+        for arc in range(self.flow.num_arcs()):
+            si = self.flow.tail(arc)
+            ci = self.flow.head(arc) - self.num_students
+            if self.flow.flow(arc) > 0 and si < self.num_students:
                 matches.append((si, ci))
-            # arcs from source to student
-            elif ci < 0 and self.flow.Flow(arc) == 0:
+            elif ci < 0 and self.flow.flow(arc) == 0:
                 rows = fixed_matches.loc[
-                    fixed_matches['Student index'] == self.flow.Head(arc)]
+                    fixed_matches['Student index'] == self.flow.head(arc)]
                 if len(rows.index) == 0 or rows.iloc[0]['Course index'] == -1:
-                    matches.append((self.flow.Head(arc), -1))
+                    matches.append((self.flow.head(arc), -1))
 
-        # put fixed matches at top and unassigned at bottom
         matches.sort(
             key=lambda tup: 100 if tup[1] == -1 else (
                 -100 if (fixed_matches == tup).all(1).any() else -weights[
@@ -132,6 +129,18 @@ class MatchingGraph:
 
     def get_slots_unfilled(self, slots_filled: List[int]) -> int:
         return self.slots - sum(slots_filled)
+
+    def graph_weight(self):
+        return -self.flow.optimal_cost() / (10 ** DIGITS)
+
+    def print(self):
+        for arc in range(self.flow.num_arcs()):
+            print(
+                "Start: {}, End: {}, Capacity: {}, Cost: {}, flow:{}".format(
+                    self.flow.tail(arc), self.flow.head(arc),
+                    self.flow.capacity(arc), self.flow.unit_cost(arc),
+                    self.flow.flow(arc)))
+
 
     def write_matching(self, filename: str, weights: np.ndarray,
                        student_data: pd.DataFrame, course_data: pd.DataFrame,
@@ -194,14 +203,4 @@ class MatchingGraph:
             optimal_cost = self.graph_weight()
             return optimal_cost, self.get_slots_unfilled(slots_filled), matches
 
-    def graph_weight(self):
-        """ Requires that the graph has been solved before """
-        return -self.flow.OptimalCost() / (10 ** DIGITS)
 
-    def print(self):
-        for arc in range(self.flow.NumArcs()):
-            print(
-                "start: {}, end: {}, capacity: {}, cost: {}, flow:{}".format(
-                    self.flow.Tail(arc), self.flow.Head(arc),
-                    self.flow.Capacity(arc), self.flow.UnitCost(arc),
-                    self.flow.Flow(arc)))
